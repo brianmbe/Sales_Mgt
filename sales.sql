eefdac7b6a70dbe42a55aa005e47e2bbb664fe04
Brian Nyingambe <briannmbe@gmail.com>
2023-01-31 15:25:12 +0300

    add more files

--- sales.sql
+++ sales.sql
@@ -1,20 +1,46 @@
-CREATE DATABASE sales_db;
+DROP TABLE sales;
 
 CREATE TABLE sales (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    date_created DATE,
-    date_fulfilled TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_created DATE DEFAULT (CURRENT_DATE),
+    date_fulfilled DATE DEFAULT (CURRENT_TIMESTAMP),
     customer_name VARCHAR(250) NOT NULL,
     product_name VARCHAR(200) NOT NULL,
-    volume INT CHECK (volume > 0),
+    volume NUMERIC(10, 2) NOT NULL CHECK (volume >= 0),
     is_recurring BOOLEAN DEFAULT FALSE,
     is_disputed BOOLEAN DEFAULT FALSE
 );
 
-INSERT INTO sales ()
+INSERT INTO sales (
+    customer_name,
+    product_name,
+    volume,
+    is_recurring
+)
 VALUES (
+    'Nyingambe Brian',
+    'Groceries',
+    7.99,
+    FALSE
 
 ),
-(),
-(),
-();
+(
+    'Mungujakisa Nixon',
+    'Microsoft 365',
+    69.99,
+    TRUE
+),
+(
+    'Trevour Bwire',
+    'Youtube Premium',
+    16.99,
+    TRUE
+),
+(
+    'Tezra Nattabi',
+    'School',
+    1500,
+    FALSE
+);
+
+SELECT * FROM sales;
